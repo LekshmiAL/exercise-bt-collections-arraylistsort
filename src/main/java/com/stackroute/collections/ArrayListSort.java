bcd526d05e007cd82d6cc01f87b1eaf91ffd51d3
Jaswanth-Stackroute <jaswanthstackroute@gmail.com>
2020-01-29 12:27:34 +0530

    initial code push

---
+++ src/main/java/com/stackroute/collections/ArrayListSort.java
@@ -0,0 +1,11 @@
+package com.stackroute.collections;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class ArrayListSort {
+    //write logic to sort given list in descending order
+    public ArrayList<BigInteger> arrayListSortDesc(ArrayList<BigInteger> list) {
+        return null;
+    }
+}
