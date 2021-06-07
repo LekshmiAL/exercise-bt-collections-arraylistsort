9865b4bd0d96caaac13714b7fbbc27bacf7ced15
Lekshmi.Anilkumar <Lekshmi.Anilkumar@ust.com>
2021-06-07 11:13:09 +0530

    hackathon arraylist sort commit

--- src/main/java/com/stackroute/collections/ArrayListSort.java
+++ src/main/java/com/stackroute/collections/ArrayListSort.java
@@ -2,10 +2,13 @@ package com.stackroute.collections;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListSort {
     //write logic to sort given list in descending order
     public ArrayList<BigInteger> arrayListSortDesc(ArrayList<BigInteger> list) {
-        return null;
+    	//sort the given arraylist in the descending order
+    	Collections.sort(list,Collections.reverseOrder());
+        return list;
     }
 }
